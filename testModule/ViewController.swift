af07fc9eda4b611a5de48ddf71aae099efb32d3a
PetrykDima <petrykdima@gmail.com>
2020-07-15 15:01:09 +0300

    Start

---
+++ testModule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testModule
+//
+//  Created by Petryk Dima on 15.07.2020.
+//  Copyright © 2020 Eltex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
